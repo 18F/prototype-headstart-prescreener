73edd86ebad2f56dd3461906dbed1679d4433bd7
Alex Soble <alex.soble@gsa.gov>
2020-02-18 11:13:14 -0600

    Implement basic HttpAuth

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
 from flask import Flask, render_template, request
+from flask_httpauth import HTTPBasicAuth
 import requests
 import json
 import os
+from os import path
+
+if path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    # Auth not required locally, just in production.
+    # Setting an AUTH_OFF flag to any value will turn off auth.
+    auth_off = os.getenv('AUTH_OFF', False)
+
+    if auth_off:
+        return True
+
+    # If auth is on, a username and password must be supplied
+    # as environment variables.
+    env_username = os.environ['USERNAME']
+    env_password = os.environ['PASSWORD']
+
+    if (username == env_username) and (password == env_password):
+        return True
+
+    return False
+
 
 @app.route("/")
+@auth.login_required
 def hello():
     return render_template('index.html')
 
